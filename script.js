c5b420beac8bc3a4f19b769727838f18a91b2e84
kndb-star <konarkdebbarma@outlook.com>
2023-09-24 21:05:47 +0530

    Updated content, added base logic for collision management, score, game over

--- script.js
+++ script.js
@@ -12,13 +12,10 @@ moon.style.animationPlayState = 'paused';
 
 sun.addEventListener('animationiteration', () => {
 
-    body.style.backgroundColor = '#000033';
+    body.style.backgroundColor = '#000055';
 
     moon.style.animationPlayState = 'running';
-    star1.style.visibility = 'visible';
-    star2.style.visibility = 'visible';
-    star3.style.visibility = 'visible';
-    star4.style.visibility = 'visible';
+    changeStarVisibility(true);
 
     sun.style.transform = 'translateX(110%)';
     sun.style.animationPlayState = 'paused';
@@ -26,21 +23,25 @@ sun.addEventListener('animationiteration', () => {
 
 moon.addEventListener('animationiteration', () => {
 
-    body.style.backgroundColor = '#87CEEB';
+    body.style.backgroundColor = '#009ACD';
 
     sun.style.animationPlayState = 'running';
-    star1.style.visibility = 'hidden';
-    star2.style.visibility = 'hidden';
-    star3.style.visibility = 'hidden';
-    star4.style.visibility = 'hidden';
+    changeStarVisibility(false);
 
     moon.style.transform = 'translateX(110%)';
     moon.style.animationPlayState = 'paused';
 });
 
+function changeStarVisibility(visibility) {
+    const stars = document.querySelectorAll('.star');
+
+    stars.forEach(function(star) {
+        star.style.visibility = visibility ? 'visible' : 'hidden';
+    });
+}
+
 const bird = document.querySelector('.bird');
 
-// Function to trigger the bird's jump
 function jump() {
     if (!bird.classList.contains('jumping')) {
         bird.classList.add('jumping');
@@ -50,41 +51,38 @@ function jump() {
     }
 }
 
-// Add a spacebar key press event listener to jump
 document.addEventListener('keydown', (e) => {
     if (e.key === ' ') {
-        jump();
+        jump(); // jump only if spacebar
     }
 });
 
-const evilTimer = setInterval(createEvil, 5000); // try to spawn a evil bird every tick
-
+const evilTimer = setInterval(createEvil, 4000); // try to spawn a evil bird every tick
 let evilCounter = 0; // global counter for evils, we will use this for bonus
 
 function createEvil() {
 
     const evilElements = document.querySelectorAll('.evil').length;
 
-    if (evilElements < 5) {
+    if (evilElements < 10) {
 
         evilCounter++;
 
-        const minTop = 20;
+        const minTop = 30;
         const maxTop = 90;
         const randomTop = Math.random() * (maxTop - minTop) + minTop;
 
-        const div = document.createElement('div'); // Create a new div element
-        div.classList.add('evil'); // Add a CSS class to style the div (optional)
+        const div = document.createElement('div'); 
+        div.classList.add('evil'); 
 
-        const fast = Math.random() < 0.5;
-        if (fast) {
+        const shouldBeFast = Math.random() < 0.4; // set close to 1 for more fast evils
+        if (shouldBeFast) {
             div.classList.add('evilfast');
         } else {
             div.classList.add('evilslow');
         }
-        div.style.top = `${randomTop}%`; // Set the top position
-        document.body.appendChild(div); // Append the div to the body
-
+        div.style.top = `${randomTop}%`; 
+        document.body.appendChild(div); 
 
         div.addEventListener('animationiteration', () => {
             div.remove();
@@ -92,31 +90,31 @@ function createEvil() {
     }
 }
 
-const bonusTimer = setInterval(createBonus, 5000);
+const bonusTimer = setInterval(createBonus, 4001); // try to spawn a bonus every tick
 
 function createBonus() {
 
-    const minTop = 20;
+    const minTop = 30;
     const maxTop = 90;
     const randomTop = Math.random() * (maxTop - minTop) + minTop;
 
-    const div = document.createElement('div'); // Create a new div element
+    const div = document.createElement('div');
     div.classList.add('bonus');
 
-    console.log('bonus' + evilCounter);
-    let shouldAdd = false;
+    let shouldAdd = true;
     if (evilCounter % 50 == 0) {
-        div.classList.add('bonus100');
-        shouldAdd = true;
-    } else if (evilCounter % 10 == 0) {
+        div.classList.add('bonus100'); 
+    } else if (evilCounter % 20 == 0) {
         div.classList.add('bonus50');
-        shouldAdd = true;
-    } else if (evilCounter % 5 == 0) {
+    } else if (evilCounter % 5 == 0 || evilCounter % 3 == 0) {
         div.classList.add('bonus10');
-        shouldAdd = true;
+    } else {
+        shouldAdd = false;
     }
 
     if (shouldAdd) {
+        
+         div.classList.add('bonusSize');
         div.style.top = `${randomTop}%`;
         document.body.appendChild(div);
 
@@ -125,3 +123,58 @@ function createBonus() {
         });
     }
 }
+
+
+function updateScore(score) {
+    const scoreElement = document.getElementById('scoreValue');
+    scoreElement.textContent = score;
+
+     if (score % 100 == 0) {
+        scoreElement.parentElement.classList.add('reached-100');
+    } else {
+        // Remove the animation class if the score changes
+        scoreElement.parentElement.classList.remove('reached-100');
+    }
+}
+
+let isAlive = setInterval(function() {
+
+      const evil = document.getElementsByClassName("evil");
+
+      let birdTop = bird.getBoundingClientRect().top;
+      let birdLeft = bird.getBoundingClientRect().left;
+
+      for (let i = 0; i < evil.length; i++) {
+        let evilTop = evil[i]?.getBoundingClientRect().top;
+        let evilLeft = evil[i]?.getBoundingClientRect().left;
+        if (evilTop >= birdTop && evilTop <= birdTop + 60 && evilLeft >= birdLeft && evilLeft <= birdLeft + 60) {
+          console.log('Game over!');
+
+          const customPopup = document.getElementById('customPopup');
+            customPopup.style.display = 'block';
+
+            // calculate simulation credits and hit API
+        }
+        }
+
+        const bonus = document.getElementsByClassName("bonus");
+
+        for (let i = 0; i < bonus.length; i++) {
+          let bonusTop = bonus[i]?.getBoundingClientRect().top;
+          let bonusLeft = bonus[i]?.getBoundingClientRect().left;
+          if (bonusTop >= birdTop && bonusTop <= birdTop + 60 && bonusLeft >= birdLeft && bonusLeft <= birdLeft + 60) {
+          	
+            const str = bonus[i]?.classList[1];
+            let score = parseInt(document.getElementById("scoreValue").textContent);
+            score += parseInt( str.match(/\d+/)[0], 10);
+            
+            updateScore(score);
+            
+            document.getElementsByClassName(str)[0].remove();
+          }
+        } 
+      }, 10);
+
+
+
+
